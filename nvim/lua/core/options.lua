98b2c953c4ebb70def4aa91be2414b41ba0bee3e
m26 <local@mail>
2023-10-25 20:19:50 +0700

    add neovide options

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -61,3 +61,16 @@ vim.opt.listchars = {
   eol = '↴',
 }
 
+-- Neovide
+if vim.g.neovide then
+  local cursor_vfx_mode = {"railgun", "torpedo", "pixiedust", "sonicboom", "ripple", "wireframe"}
+  vim.g.neovide_cursor_vfx_mode = cursor_vfx_mode[6]
+  vim.o.guifont = 'JetBrains Mono Nerd Font:h14'
+  vim.g.neovide_transparency = 0.8
+  -- vim.g.neovide_fullscreen = true  -- windowed fullscreen mode
+  vim.cmd([[ nnoremap <C-A-m> :let g:neovide_fullscreen = !g:neovide_fullscreen<CR> ]])
+  vim.g.neovide_cursor_animation_length = 0.08  -- 0.06
+  vim.g.neovide_cursor_trail_size = 0.8  -- 0.7
+  -- vim.g.neovide_cursor_antialiasing = false  -- Disabling may fix some cursor visual issues.
+end
+
