0a77c11fbb40aa787fb466c3716ce82bfce918d3
m26 <local@mail>
2023-10-26 06:10:02 +0700

    update options.lua

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -65,7 +65,7 @@ vim.opt.listchars = {
 if vim.g.neovide then
   local cursor_vfx_mode = {"railgun", "torpedo", "pixiedust", "sonicboom", "ripple", "wireframe"}
   vim.g.neovide_cursor_vfx_mode = cursor_vfx_mode[6]
-  vim.o.guifont = 'JetBrains Mono Nerd Font:h14'
+  vim.o.guifont = 'JetBrainsMono Nerd Font:h11'
   vim.g.neovide_transparency = 0.95
   -- vim.g.neovide_fullscreen = true  -- windowed fullscreen mode
   vim.cmd([[ nnoremap <C-A-m> :let g:neovide_fullscreen = !g:neovide_fullscreen<CR> ]])
