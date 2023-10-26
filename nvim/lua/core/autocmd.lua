69f95195aa0c66110ac2a1f3f936a03744e666dc
m26 <local@mail>
2023-10-26 13:42:50 +0700

    update autocmd.lua

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -4,10 +4,10 @@
 vim.cmd([[
 augroup options_filetype
   autocmd!
-  autocmd FileType lua set shiftwidth=2 tabstop=2 softtabstop=2
-  autocmd FileType c set shiftwidth=8
-  autocmd FileType cs set shiftwidth=4 tabstop=4 softtabstop=4
-  autocmd FileType python set shiftwidth=4 tabstop=4 softtabstop=4
+  autocmd FileType lua setlocal shiftwidth=2 tabstop=2 softtabstop=2
+  autocmd FileType c setlocal shiftwidth=8
+  autocmd FileType cs setlocal shiftwidth=4 tabstop=4 softtabstop=4
+  autocmd FileType python setlocal shiftwidth=4 tabstop=4 softtabstop=4
 augroup END
 ]])
 
