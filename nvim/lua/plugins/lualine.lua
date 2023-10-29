9a76c8d59f6b13fbf1f80efab4f135bd980a38dc
u2204 <u2204@local>
2023-10-29 09:55:50 +0700

    fix6 - them nvim-colorizer.lua

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,6 +2,9 @@ return {
   "nvim-lualine/lualine.nvim",
   --event = "BufEnter", lazy = true,
   event = "VeryLazy",
-  enabled = false,
-  opts = {},
+  enabled = true,
+  config = function()
+    vim.opt.showmode = false
+    require("lualine").setup({})
+  end
 }
